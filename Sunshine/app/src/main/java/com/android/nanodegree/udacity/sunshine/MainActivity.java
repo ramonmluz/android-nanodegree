6cbdfbffda1f40105855adc173605e7285d0d724
ramon-mota <rmluzz@gmail.com>
2017-03-09 00:21:55 -0300

    Atualização do arquivo Sunshine

--- Sunshine/app/src/main/java/com/android/nanodegree/udacity/sunshine/MainActivity.java
+++ Sunshine/app/src/main/java/com/android/nanodegree/udacity/sunshine/MainActivity.java
@@ -1,12 +1,19 @@
 package com.android.nanodegree.udacity.sunshine;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,13 +35,41 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+
+        switch(item.getItemId()){
+            case R.id.action_settings:
+                startActivity(new Intent(this, SettingsActivity.class));
+                return true;
+            case R.id.action_map:
+                openPreferredLocationInMap();
+                return true;
+            default:  return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
+    public void openPreferredLocationInMap() {
+
+        // Obtem as preferências do Usuário
+        SharedPreferences sharePrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String location = sharePrefs.getString(getString(R.string.pref_location_key), getString(R.string.pref_location_default));
+
+        // geoLocation
+        Uri geoLocation = Uri.parse("geo:0,0?").buildUpon()
+                // Adiciona query parameter
+                .appendQueryParameter("q", location)
+                .build();
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geoLocation);
+
+        // Verifica se activivit pode ser resolvida
+        if(intent.resolveActivity(getPackageManager()) != null){
+            startActivity(intent);
+        }else{
+            Log.e(LOG_TAG, "Couldn't call" + location );
+        }
+    }
+
+
 }
